9ed3a39b948bf87262b9eee9715bf88a83683885
SCRIPTS 2021 <69657855+lacasitamx@users.noreply.github.com>
2021-07-04 22:00:44 -0500

    Update senharoot.sh

--- senharoot.sh
+++ senharoot.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 clear
 # By crazy_vpn
 # Pequeno script para permissao de autenticacao root!!
-
+sudo -i
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
